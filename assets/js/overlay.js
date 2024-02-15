7e202918a732468e36c7a9b3757eb7b542690f7c
ProgrammerPeter2 <66070387+ProgrammerPeter2@users.noreply.github.com>
2024-02-15 08:23:18 +0000

    fixed the mulit overlay apperaring error

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,5 +1,6 @@
 function init_overlay(selector, title){
     let overlay = $(selector);
+    $(overlay).addClass("inactive-overlay");
     $(overlay).prepend('<div class="overlay-head"><h3>'+title+'</h3><button class="close_btn">&times;</button></div>')
     $(selector + " .close_btn").on('click', () => hide_overlay(selector));
 }
@@ -7,11 +8,15 @@ function init_overlay(selector, title){
 function show_overlay(selector){
     $(selector).parent().addClass("active-bg");
     $(selector).parent().removeClass("inactive-bg");
+    $(selector).addClass("active-overlay");
+    $(selector).removeClass("inactive-overlay");
     $(selector).show();
 }
 
 function hide_overlay(selector){
     $(selector).parent().removeClass("active-bg");
     $(selector).parent().addClass("inactive-bg");
+    $(selector).removeClass("active-overlay");
+    $(selector).addClass("inactive-overlay");
     $(selector).parent().hide();
 }
