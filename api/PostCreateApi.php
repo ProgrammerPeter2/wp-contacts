881081620b3ae68c084d989c76ff92291b58fc12
ProgrammerPeter2 <66070387+ProgrammerPeter2@users.noreply.github.com>
2024-02-19 10:17:33 +0000

    fixed post creation bugs

--- api/PostCreateApi.php
+++ api/PostCreateApi.php
@@ -15,11 +15,11 @@ class PostCreateApi extends BaseApi {
         if(!(array_key_exists("name", $json_payload) && array_key_exists("category", $json_payload) && array_key_exists("slug", $json_payload))){
             return new WP_REST_Response(array("error" => "Required parameters are missing!"), 400);
         }
-        $category = $this->db->get_category_by_slug($json_payload["slug"]);
+        $category = $this->db->get_category_by_slug($json_payload["category"]);
         if(!$category) return new WP_REST_Response(array("error" => "Category doesn't exists!"), 400);
-        $post_to_create = new Post(0, $json_payload["name"], $category->id, $json_payload("slug"));
+        $post_to_create = new Post(0, $json_payload["name"], $category->id, $json_payload["slug"]);
         if(!$this->db->check_post_exists($post_to_create)) return new WP_REST_Response(array("error" => "The post already exists!"), 409);
-        $post_to_create = $this->db->create_category($category_to_create);
+        $post_to_create = $this->db->create_post($post_to_create);
         return new WP_REST_Response(array("result" => $post_to_create), (!$post_to_create) ? 500 : 201);
     }
 }
